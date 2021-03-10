a4d065b7e2c075e31be1124a48e6c07e3605fc1b
nurgul2002 <74358247+nurgul2002@users.noreply.github.com>
2021-03-10 11:11:41 +0600

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,20 @@
+package com.sss.model;
+ 
+public class Srudent {
+	
+	private Integer studentId;
+	private String age;
+	
+	public Integer getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(Integer studentId) {
+		this.studentId = studentId;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+}
